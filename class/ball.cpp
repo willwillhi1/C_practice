0a70ec3ee3c20c288d64860c538bdd938fd6abe2
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2021-02-23 15:12:08 +0800

    Create ball.cpp

---
+++ class/ball.cpp
@@ -0,0 +1,45 @@
+#include "ball.h"
+#include <iostream>
+using namespace std;
+const float pi = 3.1415;
+
+//ball's functions 
+ball::ball()
+{
+    weight = new float;
+    height = new float;
+    *weight = 1.0;
+    *height = 1.0;
+}
+void ball::show()
+{
+    cout << "I am ball" << endl;
+}
+void ball::set_WH(float m,float n)
+{
+    weight = new float;
+    height = new float;
+    *weight = m;
+    *height = n;
+}
+float ball::sphere()
+{
+    float r = *height/2;
+    return 4*pi*r*r;
+}
+ball::~ball()
+{
+    delete weight;
+    delete height;
+}
+
+//basketball's functions
+float basketball::volume()
+{
+    float  r = *height/2;
+    return 4/3*pi*r*r*r;
+}
+void basketball::show()
+{
+    cout << "I am basketball" << endl;
+}
