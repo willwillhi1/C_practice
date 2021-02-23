0fe431b929ca147e4fdf35de634aab83f5f3bd5b
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2021-02-23 15:12:33 +0800

    Create ball.h

---
+++ class/ball.h
@@ -0,0 +1,22 @@
+class ball{
+    protected:
+        float *weight;
+        float *height;
+    public:
+        ball();
+        virtual void show();
+        void set_WH(float,float);
+        float sphere();
+        ~ball();
+};
+
+class basketball:public ball
+{
+    public:
+        //basketball();
+        //void set_WH(float,float);
+        //float sphere();
+        //~basketball();
+        float volume();
+        void show();
+};
