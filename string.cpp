6c8d234e730e4a8fab6ae7f4acb2a7d55268a27f
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2022-02-09 16:29:47 +0800

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int
+main ()
+{
+  string s1, s2, s3;
+  s1 = "hello world";
+  s2.assign (s1, 0, 5);
+  cout << "s2: " << s2 << endl;
+  //cout << s1 << endl;
+  //s1 = "";
+  /*
+     char a[5];
+     strcpy(a, s1.c_str());
+     cout << s1[1] << endl;
+   */
+  string s4;
+  s3.assign (s1, 5, s1.size ());
+  s4.append (s2);
+  s4.append (s3);
+  cout << s4 << endl;
+
+  //find index of s3 in s1
+  cout << "index of s3 in s1: " << s1.find (s3, 0) << endl;
+
+  //insert
+  s4.insert (s4.size (), "!!");
+  cout << "insert '!!': " << s4 << endl;
+
+  //length of string
+  cout << "string length: " << s4.size () << endl;
+
+  s4.erase(s4.find("!", 0), s4.size());
+  cout<< "erase '!': " << s4 << endl;
+  
+  s4 = s1 + "!!"; 
+  
+  s4.replace(s4.find("!", 0), s4.size()-1, "!!!");
+  cout << "replace '!!' to '!!!': " << s4 << endl;
+
+  return 0;
+}
