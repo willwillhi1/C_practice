7c7a264516d717c2566df443505e652934dfc36a
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2022-03-15 15:07:14 +0800

    Create quick_sort.c

---
+++ quick_sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap(int *a, int *b) 
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int partition(int *arr, int front, int end) 
+{
+    int pivot = end;
+    int i = front - 1;
+    int j = front;
+    while (j != end) 
+    {
+        if (arr[j] < arr[pivot]) 
+        {
+            i++;
+            swap(arr+i, arr+j);
+        }
+        j++;
+    }
+    i++;
+    swap(arr+i, arr+pivot);
+    return i;
+}
+
+void quicksort(int *arr, int front, int end) 
+{
+    if (front < end) 
+    {
+        int pivot = partition(arr, front, end);
+        quicksort(arr, front, pivot-1);
+        quicksort(arr, pivot+1, end);
+    }
+}
+
+int main()
+{
+    //int arr[] = {4, 1, 9, 6, 7, 3, 8, 2, 5};
+    int *arr = malloc(sizeof(int)*9);
+    arr = {4, 1, 9, 6, 7, 3, 8, 2, 5};
+    quicksort(&arr[0], 0, 8);
+    for (int i = 0; i < 9; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
